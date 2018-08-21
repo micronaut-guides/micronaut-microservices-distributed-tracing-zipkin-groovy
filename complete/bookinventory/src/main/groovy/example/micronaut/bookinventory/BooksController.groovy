0014396eed8a3e096ad079ecaaeaa34df5d2f61a
sdelamo <sergio.delamo@softamo.com>
2018-08-21 15:31:15 +0200

    Initial Guide

---
+++ complete/bookinventory/src/main/groovy/example/micronaut/bookinventory/BooksController.groovy
@@ -0,0 +1,34 @@
+package example.micronaut.bookinventory
+
+import groovy.transform.CompileStatic
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import io.micronaut.tracing.annotation.ContinueSpan
+import io.micronaut.tracing.annotation.SpanTag
+import io.micronaut.validation.Validated
+import javax.validation.constraints.NotBlank
+
+@CompileStatic
+@Validated
+@Controller("/books")
+class BooksController {
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get("/stock/{isbn}")
+    @ContinueSpan // <1>
+    Boolean stock(@SpanTag("stock.isbn") @NotBlank String isbn) { // <2>
+        bookInventoryByIsbn(isbn).map { bi -> bi.getStock() > 0 }.orElse(null)
+    }
+
+    private Optional<BookInventory> bookInventoryByIsbn(String isbn) {
+        if(isbn == "1491950358") {
+            return Optional.of(new BookInventory(isbn, 4))
+
+        } else if(isbn == "1680502395") {
+            return Optional.of(new BookInventory(isbn, 0))
+        }
+        Optional.empty()
+    }
+}
